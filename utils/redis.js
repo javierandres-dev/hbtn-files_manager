693c493b84eccbb71fd45661ac0d0b5559899915
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-27 12:18:51 -0500

    Task 0

--- utils/redis.js
+++ utils/redis.js
@@ -4,35 +4,26 @@ import { promisify } from 'util';
 class RedisClient {
   constructor() {
     this.client = redis.createClient();
-    this.getAsync = promisify(this.client.get).bind(this.client);
-
-    this.client.on('error', (error) => {
-      console.log(`Redis client not connected to the server: ${error.message}`);
-    });
-
-    this.client.on('connect', () => {
-      //   console.log('Redis client connected to the server');
-    });
+    this.clientGet = promisify(this.client.get).bind(this.client);
+    this.clientSetEx = promisify(this.client.setex).bind(this.client);
+    this.clientDel = promisify(this.client.del).bind(this.client);
   }
-
+  
   isAlive() {
     return this.client.connected;
   }
 
-  async get(key) {
-    const value = await this.getAsync(key);
-    return value;
-  }
-
   async set(key, value, duration) {
-    this.client.setex(key, duration, value);
+    return await this.clientSetEx(key, duration, value);
   }
 
-  async del(key) {
-    this.client.del(key);
+  async get(key) { 
+    return await this.clientGet(key);
+  }
+
+  async del() {
+    return await this.clientDel(key);
   }
 }
-
 const redisClient = new RedisClient();
-
-export default redisClient;
+module.exports = redisClient;
