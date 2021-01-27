6e2dc83ab4a4549a45765deceb5c7c2b3fafdc00
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-27 13:21:39 -0500

    Task 2

---
+++ routes/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import AppController from '../controllers/AppController';
+import UsersController from '../controllers/UsersController';
+import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
+
+function controllerRouting(app) {
+  const router = express.Router();
+  app.use('/', router);
+
+  router.get('/status', (req, res) => {
+    AppController.getStatus(req, res);
+  });
+
+  router.get('/stats', (req, res) => {
+    AppController.getStats(req, res);
+  });
+}
+
+export default controllerRouting;
