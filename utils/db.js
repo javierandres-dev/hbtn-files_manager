9d280e2cbebd4059842d3c066d9657361950ca59
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2021-01-27 12:44:28 -0500

    Task 1

---
+++ utils/db.js
@@ -0,0 +1,35 @@
+import { MongoClient } from 'mongodb';
+
+const DB_HOST = process.env.DB_HOST || 'localhost';
+const DB_PORT = process.env.DB_PORT || 27017;
+const DB_DATABASE = process.env.DB_DATABASE || 'files_manager';
+const url = `mongodb://${DB_HOST}:${DB_PORT}`;
+
+class DBClient {
+  constructor() {
+    MongoClient.connect(url, { useUnifiedTopology: true }, (error, client) => {
+      if (error) {
+        console.log(err.message);
+        this.db = false;
+        return;
+      }
+      this.db = client.db(DB_DATABASE);
+      this.users = this.db.collection('users');
+      this.files = this.db.collection('files');
+    });
+  }
+
+  isAlive() {
+    return !!this.db;
+  }
+
+  async nbUsers() {
+    return this.users.countDocuments();
+  }
+
+  async nbFiles() {
+    return this.files.countDocuments();
+  }
+}
+const dbClient = new DBClient();
+export default dbClient;
